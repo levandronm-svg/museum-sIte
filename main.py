375c2d8954abb163489488de33b0ee61964e395f
LevMolchanov <levandron.m@gmail.com>
2026-03-14 17:52:29 +0300

    Убрал debug. Добавил host и port

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("v2/index.html")
+
+
+@app.route("/day-<num>")
+def day(num):
+    return render_template("day-" + num + ".html")
+
+@app.route("/exhibit-<num>")
+def exhibit(num):
+    return render_template("v2/exhibit-" + num + ".html")
+
+
+@app.route("/photo-<num>")
+def photo(num):
+    return render_template("photo-" + num + ".html")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80)
